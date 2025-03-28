c34ab82bca1e07d63daf267f912e506d7babb791
Leonardo Arias Esparza <leo@MacBook-Air-de-Leo.local>
2025-03-27 19:08:13 -0700

    Can plot PE

--- plot_utils.py
+++ plot_utils.py
@@ -29,22 +29,73 @@ def calculate_grid_values(path,op=0,rows_to_skip=0):
     line_data = line_data[fs:fe]
     line_indexes = get_signal_indexes_numpy(line_data)
     # RESOLUTION
-    res = int(line_indexes.size/2) - 1
+    res = line_indexes.size//2
     # PIXEL
-    line_indexes_stack = np.reshape(line_indexes,(res+1,2))
+    line_indexes = np.reshape(line_indexes, (res,2))
     pixel_data = pixel_data[fs:fe]
-    pixel_indexes = get_signal_indexes_numpy(pixel_data)
-    pixel_indexes_stack = np.array(
-        [
-            [pixel_indexes[pixel_indexes >= start][0],pixel_indexes[pixel_indexes <= end][-1]] for start,end in line_indexes_stack
-            ])
-    pixel_indexes_stack = pixel_indexes_stack[1:]
+    pixel_indexes = np.array([np.linspace(s,s+(e-s)//res*res,res+1) for s,e in line_indexes]) # DATA TRIM
+    pixel_indexes = pixel_indexes.astype(int)
     if op == 0:
         # HEIGHT
         height_data = height_data[fs:fe]
-        Z = np.mean(np.array([np.hsplit(height_data[index_stack[0]:index_stack[0]+(17000//res)*res],res) for index_stack in pixel_indexes_stack]),axis=2) # REV
-        Z = remove_linear_trend(Z)
+        height = np.zeros((res,res))
+        for i in range(res):
+            for j in range(res):
+                ps = pixel_indexes[i,j]
+                pe = pixel_indexes[i,j+1]
+                height[i,j] = np.mean(height_data[ps:pe])
+        Z = remove_linear_trend(height)
+    elif op == 1:
+        # AMP & PHASE
+        amp_data = data["Dev1/ai4"].to_numpy()[fs:fe]
+        phase_data = data["Dev1/ai5"].to_numpy()[fs:fe]
+        amps = np.zeros((res,res))
+        phases = np.zeros((res,res))
+        for i in range(res):
+            for j in range(res):
+                ps = pixel_indexes[i,j]
+                pe = pixel_indexes[i,j+1]
+                amps[i,j] = np.max(amp_data[ps:pe])
+                phases[i,j] = phase_data[np.argmax(amp_data[ps:pe])]
+        Z = amps
     return Z
+
+def calculate_grid_values_amp(path, rows_to_skip=0): 
+        # ------- CARGAR DATOS -------
+        # file_path = os.path.join(desktop_path, ("pe_data.csv"))
+        # data = load_data(file_path,rows_to_skip=4)
+        # data = data.to_numpy()
+        data = np.load(path)
+        # -------  FIN -------
+        # FRAME
+        frame_data = data[:,0]
+        frame_indexes = get_signal_indexes_numpy(frame_data)
+        fs = frame_indexes[1]
+        fe = frame_indexes[2]
+        # LINE
+        line_data = data[:,1][fs:fe]
+        line_indexes = get_signal_indexes_numpy(line_data)[1:-1]
+        line_indexes = np.reshape(line_indexes, (line_indexes.size//2,2))
+        res = line_indexes.shape[0]
+        # PIXEL
+        pixel_data = data[:,2][fs:fe]
+        pixel_indexes = np.array([np.linspace(s,s+(e-s)//res*res,res+1) for s,e in line_indexes]) # DATA TRIM
+        pixel_indexes = pixel_indexes.astype(int)
+        # f SWEEP
+        sweep_data = data[:,3][fs:fe]
+        # AMP & PHASE
+        amp_data = data[:,4][fs:fe]
+        phase_data = data[:,5][fs:fe]
+        amps = np.zeros((res,res))
+        phases = np.zeros((res,res))
+        for i in range(res):
+            for j in range(res):
+                ps = pixel_indexes[i,j]
+                pe = pixel_indexes[i,j+1]
+                amps[i,j] = np.max(amp_data[ps:pe])
+                phases[i,j] = phase_data[np.argmax(amp_data[ps:pe])]
+        Z = amps
+        return Z
 
 def remove_linear_trend(Z):
     for i in range(len(Z)):
--- visualize.py
+++ visualize.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import(
+    QComboBox,
     QFileDialog,
     QHBoxLayout,
     QLabel,
@@ -10,7 +11,7 @@ from PySide6.QtWidgets import(
 )
 from PySide6.QtCore import QThreadPool
 from threading import Thread
-from plot_utils import calculate_grid_values
+from plot_utils import calculate_grid_values, calculate_grid_values_amp
 # from plot_dialog import PlotDialog
 from utils import Paths
 import pyqtgraph as pg
@@ -25,11 +26,13 @@ class VisualizeTab(QWidget):
         # WIDGETS
         self.path_input = QLineEdit()
         choose_path_btn = QPushButton("Select Data")
-        self.canvas_widget = gl.GLViewWidget()
+        self.plot_op = QComboBox()
+        self.plot_widget = pg.PlotWidget()
         # self.canvas_widget.setBackgroundColor("#edecec")
-        self.canvas_widget.setBackgroundColor("black")
+        self.plot_widget.setBackgroundColor("black")
 
         # CONFIG
+        self.plot_op.addItems(["Topography", "PE"])
         self.path_input.setEnabled(False)
 
         # SIGNALS
@@ -41,18 +44,16 @@ class VisualizeTab(QWidget):
         x_layout1.addWidget(QLabel("Filename:"))
         x_layout1.addWidget(self.path_input)
         x_layout1.addWidget(choose_path_btn)
+        x_layout2 = QHBoxLayout()
+        x_layout2.addWidget(QLabel("Study:"))
+        x_layout2.addWidget(self.plot_op)
         layout.addLayout(x_layout1)
-        layout.addWidget(self.canvas_widget)
+        layout.addLayout(x_layout2)
+        layout.addWidget(self.plot_widget)
         self.setLayout(layout)
 
         # TEST
-        test_y_value_input = QSpinBox()
-        layout.addWidget(test_y_value_input)
-        test_y_value_input.setRange(0,63)
-        test_y_value_input.valueChanged.connect(self.test_create_profile_plot)
-        self.z = None
-        self.is_topo_plot = False
-        self.test_plot = None
+        
 
     def get_pathname(self):
         path_data = QFileDialog.getOpenFileName()
@@ -63,18 +64,23 @@ class VisualizeTab(QWidget):
             plot_thread.start()
     
     def create_plot(self, path):
-        Z = calculate_grid_values(path,rows_to_skip=3)
+        op = self.plot_op.currentIndex()
+        Z = calculate_grid_values(path,op=op,rows_to_skip=3)
         x = np.arange(Z.shape[0])
         y = np.arange(Z.shape[0])
         X,Y = np.meshgrid(x,y)
-        cmap = plt.get_cmap('binary')
+        cmap = plt.get_cmap('pink')
         minZ=np.min(Z)
         maxZ=np.max(Z)
         rgba_img = cmap((Z-minZ)/(maxZ - minZ))
         surf_plot = gl.GLSurfacePlotItem(x,y,Z, colors=rgba_img)
-        self.canvas_widget.addItem(surf_plot)
-        self.canvas_widget.orbit(45,90)
-        self.canvas_widget.pan(dx=x.max()/2,dy=y.max()/2,dz=60)
+        self.plot_widget.addItem(surf_plot)
+        if op == 0:
+            self.plot_widget.orbit(45,90)
+            self.plot_widget.pan(dx=x.max()/2,dy=y.max()/2,dz=60)
+        else:
+            self.plot_widget.orbit(135,90)
+            self.plot_widget.pan(dx=x.max()/2,dy=y.max()/2,dz=150)
         # TEST
         self.z = Z
         self.is_topo_plot = True
