25d667209fa7745c1223da99d359c0947c9baa34
Leo Arias <leoariase99@gmail.com>
2025-04-27 15:25:14 -0700

    Added plot range handling + changed colors to QPens

--- acquire.py
+++ acquire.py
@@ -16,7 +16,7 @@ from PySide6.QtWidgets import(
     QWidget
 )
 from PySide6.QtCore import QTimer
-from PySide6.QtGui import QIcon
+from PySide6.QtGui import QIcon, QPen
 from acquisition_thread import AcquisitionThread
 # from widgets import ConnectionStatusIndicator
 from utils import Paths
@@ -43,16 +43,12 @@ class AcquireTab(QWidget):
         self.max_input_value_input = QDoubleSpinBox()
         self.sample_rate_input = QDoubleSpinBox()
         self.n_samples_input = QSpinBox()
-        # self.n_cicles_input = QSpinBox()
-        # self.n_cicles = QLabel()
         ## RECORDING CONTROLS
-        # self.filename_input = QLineEdit(text="datos_daq.csv")
         self.record_btn = QPushButton("RECORD")
         stop_btn = QPushButton("STOP")
         
         # ---- DATA ---- #
         self.channels_data = np.array([])
-        # self.plot_data = np.array([])
         self.plot_data = np.zeros((1,50000))
         self.plots_refs = [self.plot_widget.plot(np.array([]), pen="y")] # CREAR PEN ANTES
 
@@ -63,11 +59,8 @@ class AcquireTab(QWidget):
         ## THREADS & TIMER
         self.plot_timer = QTimer(self)
         self.plot_timer.timeout.connect(self.update_plot)
-        ## PLOT
-        # self.plot_widget.setXRange(1,100)
-        # self.plot_widget.setYRange(-0.4,0.4)
         ## INPUTS
-        self.n_channels_input.setRange(-10,10)
+        self.n_channels_input.setRange(1,8)
         self.n_channels_input.setValue(1)
         self.channel_selection_input.addItems(["All","Dev1/ai0", "Dev1/ai1", "Dev1/ai2", "Dev1/ai3", "Dev1/ai4", "Dev1/ai5"])
         self.sample_rate_input.setRange(0,100000)
@@ -87,6 +80,8 @@ class AcquireTab(QWidget):
         self.n_channels_input.valueChanged.connect(self.set_n_of_channels)
         self.min_input_value_input.valueChanged.connect(self.set_max_input)
         self.record_btn.clicked.connect(self.start_recording)
+        self.sample_rate_input.valueChanged.connect(lambda sample_rate: self.n_samples_input.setRange(0, sample_rate))
+        self.sample_rate_input.valueChanged.connect(self.set_plot_xrange)
         stop_btn.clicked.connect(self.stop_recording)
 
         # ---- LAYOUT ---- #
@@ -101,8 +96,6 @@ class AcquireTab(QWidget):
         f2.addRow("DAQ Sample Rate (samples/s)", self.sample_rate_input)
         f2.addRow("Number of Samples (samples per read)", self.n_samples_input)
         daq_config_group_box.setLayout(f2)
-        # form.addRow("Number of Cicles", self.n_cicles_input)
-        # form.addRow("Filename", self.filename_input)
         ## MAIN LAYOUT
         layout = QVBoxLayout()
         layout.addWidget(self.plot_widget)
@@ -120,8 +113,12 @@ class AcquireTab(QWidget):
         arr = [[] for _ in range(n)]
         self.channels_data = np.array(arr)
         self.plot_data = np.zeros((n,50000))
-        colors = ["yellow","green","blue","red", "white"] # CAMBIAR A PEN
-        self.plots_refs = [self.plot_widget.plot(np.array([]), pen=colors[i]) for i in range(n)]
+        pens = [QPen("yellow"),QPen("green"),QPen("blue"),QPen("red"), QPen("white"), QPen("pink"), QPen("orange"), QPen("gray")] # CAMBIAR A PEN
+        self.plots_refs = [self.plot_widget.plot(np.array([]), pen=pens[i]) for i in range(n)]
+
+    def set_plot_xrange(self, lim):
+        n = self.n_channels_input.value()
+        self.plot_data = np.zeros((n,int(lim*1.1)))
 
     def set_max_input(self,min_v):
         self.max_input_value_input.setMinimum(min_v)
